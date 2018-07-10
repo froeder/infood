944100f60c324a632e77482bf6b2e28b870056ab
Jhonatan Froeder <froeder3@gmail.com>
2018-07-10 14:05:20 -0400

    Inclusão inicial

---
+++ pages/buscar.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <v-card>
+        <v-card-title>Welcome to the Vuetify + Nuxt.js template</v-card-title>
+        <v-card-text v-for="jsonAlimento in alimentos" :key="jsonAlimento._id">
+          <v-btn>{{jsonAlimento.descricao}}</v-btn>
+        </v-card-text>
+        <v-card-actions>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import jsonAlimento from '../assets/alimentos.json'
+
+export default {
+  data () {
+    return {
+      alimentos: []
+    }
+  },
+  created () {
+    for (var i = 0; i < jsonAlimento.length; i++) {
+      this.alimentos[i] = jsonAlimento[i]
+    }
+    console.log(this.alimentos)
+  }
+}
+</script>
+
