328c5ef3e24971430671be39e140c5a256c3f67a
Jhonatan Froeder <froeder3@gmail.com>
2018-07-10 15:01:46 -0400

    Alterações na exibição

--- pages/buscar.vue
+++ pages/buscar.vue
@@ -1,15 +1,29 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <v-card>
-        <v-card-title>Welcome to the Vuetify + Nuxt.js template</v-card-title>
-        <v-card-text v-for="jsonAlimento in alimentos" :key="jsonAlimento._id">
-          <v-btn>{{jsonAlimento.descricao}}</v-btn>
-        </v-card-text>
-        <v-card-actions>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
+  <v-layout column align-center>
+    <v-card>
+      <v-card-title>Busque o alimentos</v-card-title>
+      <v-card-text>
+        <v-layout row wrap>
+          <v-flex xs6>
+            <v-text-field v-model="filtro" prepend-icon="search" label="Buscar"></v-text-field>
+          </v-flex>
+          <v-flex xs6>
+            <v-btn color="success" @click="buscar()" block>Buscar</v-btn>
+          </v-flex>
+        </v-layout>
+        <v-layout row wrap>
+          <v-flex md12 xs12>
+            <v-btn 
+              v-for="jsonAlimento in alimentos" 
+              :key="jsonAlimento._id">
+              {{jsonAlimento.descricao}}
+            </v-btn>
+          </v-flex>
+        </v-layout>
+      </v-card-text>
+      <v-card-actions>
+      </v-card-actions>
+    </v-card>
   </v-layout>
 </template>
 
@@ -19,7 +33,8 @@ import jsonAlimento from '../assets/alimentos.json'
 export default {
   data () {
     return {
-      alimentos: []
+      alimentos: [],
+      filtro: ''
     }
   },
   created () {
@@ -27,6 +42,11 @@ export default {
       this.alimentos[i] = jsonAlimento[i]
     }
     console.log(this.alimentos)
+  },
+  methods: {
+    buscar () {
+      console.log(this.filtro)
+    }
   }
 }
 </script>
