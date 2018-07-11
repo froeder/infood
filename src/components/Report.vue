4ccd86e7ea27cb6a9ff2c607e1dcd7e5f3091ae0
Jhonatan Froeder <froeder3@gmail.com>
2018-07-11 16:36:30 -0400

    Inclusão página report

---
+++ src/components/Report.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+      <v-layout column align-center>
+        <p><strong>Versão:</strong> 1.0.4</p>
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from '@/components/Home'
 import Buscar from '@/components/Buscar'
 import Detalhe from '@/components/_Detalhe'
+import Report from '@/components/Report'
 
 Vue.use(Router)
 
@@ -10,7 +11,7 @@ export default new Router({
   routes: [
     { path: '/', name: 'Home', component: Home },
     { path: '/buscar', name: 'Buscar', component: Buscar },
-    { path: '/alimentos/:id', name: 'Detalhe', component: Detalhe
-    }
+    { path: '/alimentos/:id', name: 'Detalhe', component: Detalhe },
+    { path: '/report', name: 'Report', component: Report }
   ]
 })
