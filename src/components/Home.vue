0716455bc5915ce6c6ca144d659d8d8ba28f5c7b
Jhonatan Froeder <froeder3@gmail.com>
2018-07-11 15:57:49 -0400

    Alterado conforme https://github.com/froeder/infood/projects/1#card-11257557

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,7 +3,7 @@
     <v-slide-y-transition mode="out-in">
       <v-layout column align-center>
         <h2>O que é?</h2>
-        <p>Projeto com informações nutricionais de vários alimentos consumidos no Brasil. <br> Foi feito usando a Taco API, do Raul de Melo que pode ser encontrada em: <a href="">Incluir Link aqui</a>  </p>
+        <p>Projeto com informações nutricionais de vários alimentos consumidos no Brasil. <br> Foi feito usando a Taco API, do Raul de Melo que pode ser encontrada em: <a href="https://github.com/raulfdm/taco-api">Taco API</a>  </p>
         <p>Mais informações, acesse: <a target="_blank" href="http://froeder.github.io">Meu portifólio</a></p>
       </v-layout>
     </v-slide-y-transition>
